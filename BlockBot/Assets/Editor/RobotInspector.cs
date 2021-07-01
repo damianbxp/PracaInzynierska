aa266cb8bcbc1c74c52d2135f9c43e7c03fadf0d
Damian Bartuś <damianb.xp@gmail.com>
2021-07-02 01:27:06 +0200

    Robot data is loaded from XML file changes done in Unity can be saved to XML

--- BlockBot/Assets/Editor/RobotInspector.cs
+++ BlockBot/Assets/Editor/RobotInspector.cs
@@ -6,26 +6,32 @@ using UnityEngine;
 [CustomEditor(typeof(Robot))]
 public class RobotInspector : Editor
 {
-
+    bool showAxis;
 
     public override void OnInspectorGUI() {
         EditorGUILayout.LabelField("asdasd");
         Robot robot = target as Robot;
 
-        if(GUILayout.Button("RESET AXIS")) GenerateAxes(robot);
-        
+        if(GUILayout.Button("Load XML")) robot.loadXMLData();
+        if(GUILayout.Button("Save XML")) robot.saveXML();
 
-        foreach(RobotAxis robotAxis in robot.axes) {
-            robotAxis.rotAxis = (RobotAxis.Axes)EditorGUILayout.EnumPopup("AXIS", robotAxis.rotAxis);
+        showAxis = EditorGUILayout.Foldout(showAxis, "Axis");
+        if(showAxis) {
+            using(new GUILayout.VerticalScope(EditorStyles.helpBox)) {
+                for(int i = 0; i < robot.axes.Count; i++){
+                    using( new GUILayout.VerticalScope(EditorStyles.helpBox)) {
+                        Robot.Axis axis = robot.axes[i];
+                        EditorGUILayout.LabelField(i.ToString());
+
+                        axis.offset = EditorGUILayout.FloatField("Offset" , axis.offset);
+                        EditorGUILayout.Space(6);
+
+                        axis.minAngle = EditorGUILayout.FloatField("Min Angle", axis.minAngle);
+                        axis.maxAngle = EditorGUILayout.FloatField("Max Angle", axis.maxAngle);
+                    }
+                }
+            }
         }
 
-    }
-
-    void GenerateAxes(Robot robot) {
-        robot.axes.Clear();
-
-        for(int i = 0; i < 6; i++) {
-            robot.axes.Add(new RobotAxis());
-        }
     }
 }
