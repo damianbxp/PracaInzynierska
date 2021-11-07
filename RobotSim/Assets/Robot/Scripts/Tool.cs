591cd40c25780bf450c4bc3ad615dd10114b33a2
damianbxp <damianb.xp@gmail.com>
2021-11-07 14:27:20 +0100

    Better tool graphical representation

--- RobotSim/Assets/Robot/Scripts/Tool.cs
+++ RobotSim/Assets/Robot/Scripts/Tool.cs
@@ -9,22 +9,28 @@ public class Tool : MonoBehaviour
     public float toolDiameter;
     public float toolHeight;
 
-    Transform TCP;
 
     public bool powerOn;
 
     void Start()
     {
-        TCP = GameObject.Find("TCP").transform;
+        UpdateTool();
     }
 
     void Update() {
-        transform.position = TCP.position;
-        //transform.rotation = TCP.rotation;
+
+        transform.rotation = Quaternion.Euler(0, 0, 0); /// utrzymuje wrzeciono pionowo <- usun¹æ
+
         if(powerOn) {
             blockMesh.GetComponent<BlockGen>().ModifyBlock(toolWorkCenter.position, toolDiameter/2, toolHeight);
             //Debug.Log(transform.position - blockMesh.transform.position);
             onTerrainModified?.Invoke();
         }
+    }
+
+    public void UpdateTool() {
+        transform.localPosition = new Vector3(toolHeight / 2, 0, 0.05f);
+        transform.Find("Cylinder").localScale = new Vector3(toolDiameter, toolHeight/2, toolDiameter);
+        toolWorkCenter.localPosition = new Vector3(0, -toolHeight / 2, 0);
     }
 }
